a8e3e05768cdda622c08bcbc0588ba5f03971923
KRITH <shaanthor.study@gmail.com>
2025-09-07 23:56:55 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,4 @@
-
-//         hidden: {
-//             opacity: 0,
-//             filter: 'blur(12px)',
-//             y: 12,
-//         },
-//         visible: {
-//             opacity: 1,
-//             filter: 'blur(0px)',
-//             y: 0,
-//             transition: {
-//                 type: 'spring',
-//                 bounce: 0.3,
-//                 duration: 1.5,
+ 1.5,
 //             },
 //         },
 //     },
