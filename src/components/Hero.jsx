9d3b174e62bc105004d4256d7fa7d5981b42dc37
KRITH <shaanthor.study@gmail.com>
2025-09-07 23:57:08 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,4 @@
-(titleNumber + 1);
-//             }
-//         }, 2000);
-//         return () => clearTimeout(timeoutId);
-//     }, [titleNumber, titles]);
-
-//     return (
-//         <>
-//             <main className="overflow-hidden bg-gradient-to-br from-amber-50 via-stone-100 to-amber-50" style={{ backgroundColor: '#F5F2E8' }}>
-//                 <div
-//                     aria-hidden
-//                     className="z-[2] absolute inset-0 pointer-events-none isolate opacity-50 contain-strict hidden lg:block">
-//                     <div
-//                         className="w-[35rem] h-[80rem] -translate-y-[350px] absolute left-0 top-0 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(276,50%,70%,.08)_0,hsla(276,50%,60%,.02)_50%,hsla(276,50%,50%,0)_80%)]" />
-//                     <div
-//                         className="h-[80rem] absolute left-0 top-0 w-56 -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(276,50%,70%,.06)_0,hsla(276,50%,60%,.02)_80%,transparent_100%)] [translate:5%_-50%]" />
-//                     <div
-//                         className="h-[80rem] -translate-y-[350px] absolute left-0 top-0 w-56 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(276,50%,70%,.04)_0,hsla(276,50%,60%,.02)_80%,transparent_100%)]" />
-//                 </div>
-//                 <section>
-//                     <div className="relative pt-24 md:pt-36">
-//                         <AnimatedGroup
-//                             variants={{
-//                                 container: {
+ {
 //                                     visible: {
 //                                         transition: {
 //                                             delayChildren: 1,
