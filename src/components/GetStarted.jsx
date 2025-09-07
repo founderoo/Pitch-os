7684c83f3090efb2d9f144302e4fbe72dbb5022c
KRITH <shaanthor.study@gmail.com>
2025-09-07 23:54:48 +0530

    Update GetStarted.jsx

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react"
 import { motion, AnimatePresence } from "framer-motion"
 import { cn } from "@/lib/utils"
 
-// Demo data
 const defaultFeatures = [
   {
     step: "Step 1",
