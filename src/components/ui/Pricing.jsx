d18cb92b226478062e6464a8e7bf5ec81e56cbca
KRITH <shaanthor.study@gmail.com>
2025-09-16 23:35:44 +0530

    Update Pricing.jsx

--- src/components/ui/Pricing.jsx
+++ src/components/ui/Pricing.jsx
@@ -1,7 +1,7 @@
 import { Check, Minus, MoveRight } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import React from "react"; // ✅ Needed for React.Fragment
+import React from "react"; 
 
 const pricingPlans = [
   {
