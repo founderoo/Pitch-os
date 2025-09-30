41d6fe87e45067672c887b64abded07c64c97a98
KRITH <shaanthor.study@gmail.com>
2025-09-30 23:58:04 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,5 @@
 
 
-
-
-
-
-
-
-
-
-
-
 import React, { useState } from "react";
 import { motion, useReducedMotion } from "framer-motion";
 import { useLocation, useNavigate } from "react-router-dom";
