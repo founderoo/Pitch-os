d92b647d69da957e23d645cf0c932db5a2e49112
KRITH <shaanthor.study@gmail.com>
2025-10-29 23:57:14 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,6 +39,7 @@
 
 
 
+
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Menu, X, User, Settings, LogOut, LayoutDashboard } from 'lucide-react';
