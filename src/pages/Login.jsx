cb55952e371d0a230649e4aea1c727a1780f52af
KRITH <shaanthor.study@gmail.com>
2026-01-12 23:51:15 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,6 +12,7 @@
 
 
 
+
 import { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { signInWithGoogle, signInWithEmailPassword } from '../firebase';
